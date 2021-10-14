7d6def42e45421c8ed0ee91ca01323f14b2ddd6e
Shahroze Abbas <28969387+shahrozeabbas@users.noreply.github.com>
2021-10-14 17:22:15 -0400

    Update fastMNN.r

--- scripts/fastMNN.r
+++ scripts/fastMNN.r
@@ -3,15 +3,19 @@ args <- commandArgs(trailingOnly=TRUE)
 pkgs <- c('data.table', 'Seurat', 'dplyr', 'ggplot2', 'future', 'SeuratWrappers', 'SeuratDisk')
 invisible(sapply(pkgs, require, character.only=TRUE))
 
-to_disk <- '/gpfs/gsfs12/users/ShernData/'
-setwd(paste0(to_disk, 'NF1TumorData/'))
-output_dir <- paste0(to_disk, 'integrate_test/fastMNN/')
+home <- '/insert/main/working/directory/'
+data_dir <- paste0(home, 'data2integrate/')
+if (!dir.exists(data_dir)) stop('data directory does not exist!')
+
+setwd(data_dir)
+output_dir <- paste0(home, 'fastMNN_output/')
+if (!dir.exists(output_dir)) dir.create(output_dir)
 
 project_list_name <- 'final_NF1_project_list.rds'
 project_list <- readRDS(project_list_name)
 
-invisible(lapply(X=list.files(path=paste0(to_disk, 'scripts_NF1'),
-                                pattern='.R', full.names=TRUE), FUN=source))
+accessory_scripts <- list.files(path=paste0(home, 'accessory_scripts'), pattern='.R', full.names=TRUE)
+invisible(lapply(X=accessory_scripts, FUN=source))
 
 
 k <- as.numeric(args[1])
